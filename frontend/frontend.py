10f5488db06db9270c57f72e22e295a720dd8bba
Pernam75 <jules.rubin@efrei.net>
2024-01-21 17:17:17 +0100

    added the pdf loader

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,6 +3,11 @@ import requests
 
 st.title("Echo Bot")
 
+# add a side bar with a file uploader only with pdf files and a submit button
+st.sidebar.title("Upload a PDF")
+uploaded_file = st.sidebar.file_uploader("Choose a PDF file", type="pdf", accept_multiple_files=True)
+submit_button = st.sidebar.button("Submit")
+
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -20,9 +25,11 @@ if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-response = requests.get("http://localhost:5000/echo", params={"input": prompt}).json()["response"]
-# Display assistant response in chat message container
-with st.chat_message("assistant"):
-    st.markdown(response)
-# Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": response})
+response = requests.get("http://localhost:5000/echo", params={"input": prompt}).json()["response"] if prompt else None
+
+if response:
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
